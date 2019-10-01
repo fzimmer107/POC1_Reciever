161c6e784cc87c5bc21beac0c455500dfa913ed4
Fabian Zimmermann <F_Zimmermann@gmx.net>
2019-10-01 21:28:10 +0200

    added app

---
+++ Assets/Scripts/CustomPhotonLauncher.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class CustomPhotonLauncher : MonoBehaviourPunCallbacks
+{
+
+    string gameVersion = "1";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Connect();
+    }
+
+    public void Connect()
+    {
+        if (!PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.GameVersion = gameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+    }
+
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+        // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+        PhotonNetwork.CreateRoom(null, new RoomOptions());
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+    }
+
+}
---
+++ Assets/Scripts/RecieverTest.cs
@@ -0,0 +1,92 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class RecieverTest : MonoBehaviour
+{
+
+    private PhotonView RPCPhotonView;
+    private Dictionary<int, MeshFilter> recievedMeshes = new Dictionary<int, MeshFilter>();
+    //private List<int> RecievedMeshesID = new List<int>();
+    private Dictionary<int, int> updateCount = new Dictionary<int, int>();
+    public GameObject meshPrefab;
+    public GameObject cameraRepPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RPCPhotonView = GetComponent<PhotonView>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    [PunRPC]
+    public void RPCTest(Vector3 position, int meshId)
+    {
+        Debug.Log($"MESH FROM MESHOBSERVER WITH ID: {meshId}: POSITION:({position.x},  y{position.y}, z{position.z})");
+    }
+
+    [PunRPC]
+    public void RecieveMeshData(Vector3[] vertices, int[] triangles, Vector2[] uvs, Vector3 position, Vector3 rotation, int meshId, Vector3 camPos, Vector3 camRot)
+    {
+
+        BuildCameraRep(camPos, camRot,meshId);
+        BuildMesh(vertices, triangles, uvs, position, rotation, meshId);
+        
+    }
+
+    private void UpdataMeshData(Vector3[] vertices, int[] triangles, Vector2[] uvs, Vector3 position, Vector3 rotation, int meshId)
+    {
+        recievedMeshes[meshId].mesh.Clear();
+        recievedMeshes[meshId].mesh.vertices = vertices;
+        recievedMeshes[meshId].mesh.triangles = triangles;
+        Debug.Log($"Updated Mesh with ID: {meshId}");
+        
+    }
+
+    private void BuildCameraRep(Vector3 pos,Vector3 rot, int meshId)
+    {
+        //instantiate prefab on recieved position, to see if camera moves in scene
+        GameObject prefab = Instantiate(cameraRepPrefab, pos, Quaternion.Euler(rot.x, rot.y, rot.z));
+        prefab.name = $"{meshId}:camera";
+    }
+
+    private void BuildMesh(Vector3[] vertices, int[] triangles, Vector2[] uvs, Vector3 position, Vector3 rotation, int meshId)
+    {
+        GameObject prefab = Instantiate(meshPrefab, position, Quaternion.Euler(rotation.x, rotation.y, rotation.z));
+        prefab.name = meshId.ToString();
+        Debug.Log($"Creating Mesh with ID: {meshId}, at POSTITION: ({position.x},{position.y}, {position.z} and ROTATION ({rotation.x},{rotation.y}, {rotation.z})");
+
+
+        Mesh recievedMesh = new Mesh();
+        recievedMesh.vertices = vertices;
+            recievedMesh.triangles = triangles;
+            recievedMesh.uv = uvs;
+            MeshFilter meshFilter = prefab.GetComponent<MeshFilter>();
+            meshFilter.mesh = recievedMesh;      
+    }
+
+    [PunRPC]
+    public void SendError(Vector3 position, int meshId)
+    {
+       Debug.Log($"MESH ID FROM EVENTDATA: {meshId} at position:({position.x},  y{position.y}, z{position.z})");
+    }
+
+
+
+    [PunRPC]
+    public void RemoveMesh(int meshId)
+    {
+       
+
+    }
+}
